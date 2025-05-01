e749bb7cfe8b40af0bb0ec0cc99ed0c746e2bd0b
MannexDera <emmanuelchi545@gmail.com>
2025-05-01 19:06:15 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                white: 'hsl(0, 100%, 100%)',
+                'purple-100': 'hsl(275, 100%, 97%)',
+                'purple-600': 'hsl(292, 16%, 49%)',
+                'purple-950': 'hsl(292, 42%, 14%)'
+            },
+            fontFamily: {
+                worksans: ['Work Sans', 'sans-serif']
+            }
+        }
+    },
+    plugins: []
+};
