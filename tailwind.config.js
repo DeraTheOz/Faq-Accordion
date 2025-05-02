74695ebeae7de7caa724ffba5bfdff59104abc45
MannexDera <emmanuelchi545@gmail.com>
2025-05-02 21:16:04 +0100

    feat: add custom theme for colors, font-family, background-images, and keyframe animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             colors: {
                 white: 'hsl(0, 100%, 100%)',
-                'purple-100': 'hsl(275, 100%, 97%)',
-                'purple-600': 'hsl(292, 16%, 49%)',
-                'purple-950': 'hsl(292, 42%, 14%)'
+                purple100: 'hsl(275, 100%, 97%)',
+                purple600: 'hsl(292, 16%, 49%)',
+                purple950: 'hsl(292, 42%, 14%)'
             },
             fontFamily: {
                 worksans: ['Work Sans', 'sans-serif']
+            },
+            backgroundImage: {
+                desktop: "url('/assets/images/background-pattern-desktop.svg')",
+                mobile: "url('/assets/images/background-pattern-mobile.svg')"
+            },
+            keyframes: {
+                expand: {
+                    from: {
+                        maxHeight: '0',
+                        paddingTop: '0',
+                        paddingBottom: '0'
+                    },
+                    to: {
+                        maxHeight: '10rem',
+                        paddingTop: '1rem',
+                        paddingBottom: '1rem'
+                    }
+                },
+                
+            },
+            animation: {
+                expand: 'expand .3s ease-out forwards',
+                
             }
         }
     },
